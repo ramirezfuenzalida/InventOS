b216e467a8568cf0d59ca840e8308dbedc69b53c
Orquesta Sinfónica W.T. <orquestasinfonicaw.t.@192.168.1.232>
2026-05-25 22:13:15 -0400

    fix: update visible APP_VERSION in Sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -16,7 +16,7 @@ import {
 const APP_LOGO_URL = `${import.meta.env.BASE_URL}logo_orquesta_sinfonica_wt.png`;
 const APP_NAME = "OSWT";
 const APP_SUBTITLE = "Orquesta Sinfónica William Taylor";
-const APP_VERSION = "1.1.01 ExeApp";
+const APP_VERSION = "1.2.01 ExeApp";
 
 interface SidebarProps {
   isMobileMenuOpen: boolean;
